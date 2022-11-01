7d6d0db9916414246d46dc5fa19eb53806863c45
Hiroko Yamaji <106212802+hirokoymj@users.noreply.github.com>
2022-11-02 00:36:16 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <h1>
         This is React app that deployed in gcp. <i>by Hiroko</i>
       </h1>
-      <h2 style={{ color: "red" }}>App deploy test</h2>
-      <h2 style={{ color: "blue" }}>App deploy test</h2>
+      <h2 style={{ color: "red" }}>App deploy test 2</h2>
+      <h2 style={{ color: "blue" }}>App deploy test 3</h2>
     </div>
   );
 }
